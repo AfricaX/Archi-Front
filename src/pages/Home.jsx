b77197c2f60db7779bc3add852a2dcc4a3d228e1
unknown <discordburner169@gmail.com>
2025-03-02 22:11:27 +0800

    Appbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 import checkAuth from "../hoc/checkToken";
+import AppBar from "../components/AppBar";
 
 function Home() {
   const user = useSelector((state) => state.auth.user);
-  
+
   return (
     <Box>
+      <AppBar />
       <Typography variant="h1">Welcome , {user?.name ?? "Guest"}</Typography>
     </Box>
   );
